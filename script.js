f1103c12b31277855110b14529b2c4e18bc0505b
Murugalakshmi muthukannan <150881194+murugalakshmi-fsd@users.noreply.github.com>
2023-12-19 19:10:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+(function startCountdown() {
+    let number = 10;
+
+    setTimeout(function () {
+        document.body.innerHTML = `<p>${number}</p>`;
+        
+        setTimeout(function () {
+            number--;
+
+            document.body.innerHTML = `<p>${number}</p>`;
+
+            setTimeout(function () {
+                number--;
+
+                document.body.innerHTML = `<p>${number}</p>`;
+                setTimeout(function () {
+                    number--;
+    
+                    document.body.innerHTML = `<p>${number}</p>`;
+                    setTimeout(function () {
+                        number--;
+        
+                        document.body.innerHTML = `<p>${number}</p>`;
+                        setTimeout(function () {
+                            number--;
+            
+                            document.body.innerHTML = `<p>${number}</p>`;
+                            setTimeout(function () {
+                                number--;
+                
+                                document.body.innerHTML = `<p>${number}</p>`;
+                                setTimeout(function () {
+                                    number--;
+                    
+                                    document.body.innerHTML = `<p>${number}</p>`;
+                                    setTimeout(function () {
+                                        number--;
+                        
+                                        document.body.innerHTML = `<p>${number}</p>`;
+                                        setTimeout(function () {
+                                            number--;
+                            
+                                            document.body.innerHTML = `<p>${number}</p>`;
+    
+                                setTimeout(function () {
+                                        document.body.innerHTML = "<p>Happy Independence Day</p>";
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+    }, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+}, 1000);
+})();
